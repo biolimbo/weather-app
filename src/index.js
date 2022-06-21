87e7953973018f34f69ecf2ba643655d5645f17a
Santiago Forero Molano <santiago.forero.molano@gmail.com>
2022-06-21 16:58:35 -0500

    feat: add navbar items and logo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Navbar from "./components/Navbar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<div
+			id="body"
+			className="bg-gray-300 bg-gradient-to-t dark:from-cyan-500 dark:to-blue-500"
+		>
+			<Navbar />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</div>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
