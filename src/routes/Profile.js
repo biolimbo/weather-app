aa96ccd92375ac65696c6a2b00c465b8b5afbaff
Santiago Forero Molano <santiago.forero.molano@gmail.com>
2022-06-23 19:39:33 -0500

    fix(profile): missing uset photo url

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import SVG from "react-inlinesvg";
 
 import { AuthContext } from "../contexts/Auth";
 function Profiles() {
@@ -15,11 +16,16 @@ function Profiles() {
 			<div className="flex flex-col rounded-xl gap-y-2 p-6 my-4 mx-4 max-w-md bg-gray-200/75 backdrop-blur-md drop-shadow-md text-sky-800 items-center">
 				<h1 className=" mb-3">Logged in as</h1>
 
-				<img
-					className="rounded-full h-24 w-24 mb-3"
-					src={user?.photoURL}
-					alt="profile"
-				/>
+				{user.photoURL ? (
+					<img
+						className="rounded-full h-24 w-24 mb-3"
+						src={user.photoURL}
+						alt="user avatar"
+					/>
+				) : (
+					<SVG src="/images/icons/user.svg" className="h-24 w-24 mb-3" />
+				)}
+
 				<p>{name}</p>
 				<p>{user?.email}</p>
 			</div>
